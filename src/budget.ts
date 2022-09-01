83f1c0ddf7b5cfa8952806f15d582cc5cfeffeb5
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2022-09-01 23:55:42 +0000

    feat: better budget config

--- src/budget.ts
+++ src/budget.ts
@@ -6,8 +6,12 @@ import {
 import { Construct } from 'constructs';
 import { AccountConfig, ControlTowerProps } from './aws-org';
 
+export interface AccountBudgetOptions {
+  readonly amount: number;
+  readonly email?: string;
+}
 export interface BudgetConfig<T extends string> {
-  readonly budgets: AccountConfig<T, number>;
+  readonly budgets: AccountConfig<T, number | AccountBudgetOptions>;
 }
 
 export type BudgetStackProps<T extends string> = BudgetConfig<T> & StackProps & ControlTowerProps<T>;
@@ -18,12 +22,17 @@ export class BudgetStack<T extends string> extends Stack {
 
     for (const accountName of Object.keys(props.budgets)) {
       const account = props.accounts[accountName as T];
+      const config = props.budgets[accountName as T]!;
+      const isOpts = (typeof config !== 'number');
+
+      const address = (isOpts ? config.email : undefined) ?? account.Email;
+
       new budgets.CfnBudget(this, `Budget${account.Name}`, {
         budget: {
           budgetType: 'COST',
           budgetLimit: {
             unit: 'USD',
-            amount: props.budgets[accountName as T]!,
+            amount: isOpts ? config.amount : config as number,
           },
           timeUnit: 'MONTHLY',
           costFilters: {
@@ -53,7 +62,7 @@ export class BudgetStack<T extends string> extends Stack {
             subscribers: [
               {
                 subscriptionType: 'EMAIL',
-                address: account.Email,
+                address,
               },
             ],
           }, {
@@ -65,7 +74,31 @@ export class BudgetStack<T extends string> extends Stack {
             subscribers: [
               {
                 subscriptionType: 'EMAIL',
-                address: account.Email,
+                address,
+              },
+            ],
+          }, {
+            notification: {
+              notificationType: 'ACTUAL',
+              comparisonOperator: 'GREATER_THAN',
+              threshold: 100,
+            },
+            subscribers: [
+              {
+                subscriptionType: 'EMAIL',
+                address,
+              },
+            ],
+          }, {
+            notification: {
+              notificationType: 'ACTUAL',
+              comparisonOperator: 'GREATER_THAN',
+              threshold: 120,
+            },
+            subscribers: [
+              {
+                subscriptionType: 'EMAIL',
+                address,
               },
             ],
           },
